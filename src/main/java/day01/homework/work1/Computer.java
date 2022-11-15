6197c700e898fed97b3e5e5fdf93331fc5563d2d
liyajun <332209078@qq.com>
2022-11-15 15:09:21 +0800

    add work

--- src/main/java/day01/homework/work1/Computer.java
+++ src/main/java/day01/homework/work1/Computer.java
@@ -9,50 +9,90 @@ package day01.homework.work1;
 
 public class Computer {
     private Screen screen;
-    private int price;
+    private double price;
     private String brand;
     private String des;
 
+
+    public Computer() {
+    }
+
+    public Computer(Screen screen, double price, String brand, String des) {
+        this.screen = screen;
+        this.price = price;
+        this.brand = brand;
+        this.des = des;
+        if (price < 1000 || price > 20000)this.price = 2000;
+    }
+
+    /**
+     * 获取
+     * @return screen
+     */
     public Screen getScreen() {
         return screen;
     }
 
+    /**
+     * 设置
+     * @param screen
+     */
     public void setScreen(Screen screen) {
         this.screen = screen;
     }
 
-    public int getPrice() {
+    /**
+     * 获取
+     * @return price
+     */
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(int price) {
+    /**
+     * 设置
+     * @param price
+     */
+    public void setPrice(double price) {
+
         this.price = price;
+        if (price < 1000 || price > 20000)this.price = 2000;
     }
 
+    /**
+     * 获取
+     * @return brand
+     */
     public String getBrand() {
         return brand;
     }
 
+    /**
+     * 设置
+     * @param brand
+     */
     public void setBrand(String brand) {
         this.brand = brand;
     }
 
+    /**
+     * 获取
+     * @return des
+     */
     public String getDes() {
         return des;
     }
 
+    /**
+     * 设置
+     * @param des
+     */
     public void setDes(String des) {
         this.des = des;
     }
 
-    @Override
     public String toString() {
-        return "Computer{" +
-                "screen=" + screen +
-                ", price=" + price +
-                ", brand='" + brand + '\'' +
-                ", des='" + des + '\'' +
-                '}';
+        return "Computer{screen = " + screen + ", price = " + price + ", brand = " + brand + ", des = " + des + "}";
     }
 }
 
--- src/main/java/day01/homework/work1/Screen.java
+++ src/main/java/day01/homework/work1/Screen.java
@@ -8,20 +8,71 @@
 package day01.homework.work1;
 
 public class Screen{
-    private Double size;
+    private int size;
+    private String type;
+    private String brand;
 
-    public Double getSize() {
+
+    public Screen() {
+    }
+
+    public Screen(int size, String type, String brand) {
+        this.size = size;
+        this.type = type;
+        this.brand = brand;
+        if (size != 17 && size != 23 && size != 19 && size != 25)this.size = 17;
+    }
+
+    /**
+     * 获取
+     * @return size
+     */
+    public int getSize() {
         return size;
     }
 
-    public void setSize(Double size) {
+    /**
+     * 设置
+     * @param size
+     */
+    public void setSize(int size) {
         this.size = size;
+        if (size != 17 && size != 23 && size != 19 && size != 25)this.size = 17;
     }
 
-    @Override
+    /**
+     * 获取
+     * @return type
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * 设置
+     * @param type
+     */
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    /**
+     * 获取
+     * @return brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * 设置
+     * @param brand
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
     public String toString() {
-        return "Screen{" +
-                "size=" + size +
-                '}';
+        return "Screen{size = " + size + ", type = " + type + ", brand = " + brand + "}";
     }
 }
