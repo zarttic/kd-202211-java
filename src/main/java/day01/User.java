032396d94b13ee2bf90e2eaffa25f6228a47a60b
liyajun <332209078@qq.com>
2022-11-15 11:20:52 +0800

    entity

---
+++ src/main/java/day01/User.java
@@ -0,0 +1,13 @@
+/**
+ * {@code @Description}
+ *
+ * @author liyajun
+ * {@code @create}          2022-11-15 11:19
+ */
+
+package day01;
+
+public class User {
+    private int id;
+    private  String username;
+}
---
+++ src/main/java/day01/userTest.java
@@ -0,0 +1,15 @@
+/**
+ * {@code @Description}
+ *
+ * @author liyajun
+ * {@code @create}          2022-11-15 11:20
+ */
+
+package day01;
+
+public class userTest {
+    public static void main(String[] args) {
+        User u = new User();
+        System.out.println(u);
+    }
+}
