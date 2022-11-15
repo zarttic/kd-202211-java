a1619c93f65bf0b0690bf22b324be69d2401174e
liyajun <332209078@qq.com>
2022-11-15 08:59:57 +0800

    init

---
+++ src/main/java/test.java
@@ -0,0 +1,12 @@
+/**
+ * @author liyajun
+ * @Description
+ * @create 2022-11-15 8:59
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello ");
+    }
+
+}
