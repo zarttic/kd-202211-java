a9bf04ac2668c492da66821b8d80b5145fc31098
liyajun <332209078@qq.com>
2022-11-15 13:41:39 +0800

    entity

--- src/main/java/day01/User.java
+++ src/main/java/day01/User.java
@@ -9,5 +9,38 @@ package day01;
 
 public class User {
     private int id;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public User(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public User(int id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
     private  String username;
 }
